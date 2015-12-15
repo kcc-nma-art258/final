289d7ac27f5f87abe7528eae891ed853f08c69fd
LeeHyela <borcha712@hotmail.com>
2015-12-15 13:43:47 -1000

    add java script for dates

--- script.js
+++ script.js
@@ -85,3 +85,43 @@ button_1.addEventListener('click', changeColor_1);
 button_2.addEventListener('click', changeColor_2);
 button_3.addEventListener('click', changeColor_3);
 button_4.addEventListener('click', changeColor_4);
+
+
+//week
+var d = new Date();
+var weekday = new Array(7);
+weekday[0]=  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.getElementById("whatweek").innerHTML = n;
+
+//month
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
+document.getElementById("whatmonth").innerHTML = n;
+
+//date
+var d = new Date();
+    var n = d.getDate();
+    document.getElementById("whatdate").innerHTML = n;
+
+//  document.getElementById("whatmonth").innerHTML = n;
